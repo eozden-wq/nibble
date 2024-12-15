026154acbbaf80b41dca4feef2047f571bdc2d7d
Emre <tlcj35@durham.ac.uk>
2024-12-15 02:36:14 +0000

    :building_construction: Made a class to help with reading and writing data to our data file

---
+++ server/core/RecipeSerializer.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * Class that handles reading and writing to data.json
+ * Motivation - The idea of loading, writing and re-loading the same huge hunk of data doesn't seem too appealing
+ * This class takes the name of the file where the data will be stored and all the reads/writes and editing will be done through this 
+ */
+
+const fs = require('fs');
+
+class RecipeSerializer {
+    constructor(data_file) {
+        this.data = "";
+        fs.readFile(data_file, (err, data) => {
+            if (err) throw err;
+
+            this.data = JSON.parse(data);
+        });
+    };   
+    
+    read() {
+        
+    }
+    
+    write() {
+        
+    }
+    
+    edit(id) {
+
+    }
+}
+
+module.exports = RecipeSerializer;
