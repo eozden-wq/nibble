dc45af3b4bcdf4469131f0c5e97d342835744955
Emre <tlcj35@durham.ac.uk>
2024-12-16 01:08:46 +0000

    :white_check_mark: Wrote more test cases for api.recipe.get endpoint

--- tests/recipe.test.js
+++ tests/recipe.test.js
@@ -6,5 +6,17 @@ describe('Testing Recipe-related API Endpoints', () => {
         test('api.recipe.get should return a 200 response for valid input id', () => {
             request(app).get('/api/recipe/get').query({"recipe_id": 0}).expect(200);
         });
+        
+        test('api.recipe.get returns 400 for out-of-index id', () => {
+            request(app).get('/api/recipe/get').query({"recipe_id": 10}).expect(400);
+        });
+        
+        test('api.recipe.get returns 400 for negative id', () => {
+            request(app).get('/api/recipe/get').query({"recipe_id": -1}).expect(400);
+        });
+        
+        test('api.recipe.get returns 400 for non-number id', () => {
+            request(app).get('api/recipe/get').query({"recipe_id": "Hello, World!"}).expect(400);
+        })
     });
 });
