0f83b773399da62a26379c4545597c5b330b5bdf
Emre Ozden <tlcj35@durham.ac.uk>
2024-12-16 17:54:43 +0000

    :white_check_mark: Added simple test for api.recipe.create endpoint - needs further testing

--- tests/recipe.test.js
+++ tests/recipe.test.js
@@ -16,7 +16,13 @@ describe('Testing Recipe-related API Endpoints', () => {
         });
         
         test('api.recipe.get returns 400 for non-number id', () => {
-            request(app).get('api/recipe/get').query({"recipe_id": "Hello, World!"}).expect(400);
-        })
+            request(app).get('/api/recipe/get').query({"recipe_id": "Hello, World!"}).expect(400);
+        });
+    });
+    
+    describe('POST /api/recipe/create', () => {
+        test('api.recipe.create should return 200 when a valid recipe is given', () => {
+            request(app).post('/api/recipe/create').send({"author": "John Doe", "dish_name": "Sticky Toffee Pudding", "instructions": "I don't know how to make Sticky toffee pudding"}).set('Content-Type', 'application/json').expect({"code": 200, "message": "Success"})
+        });
     });
 });
