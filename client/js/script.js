00c8dba4672a50ecbc49db827ac8b38269ea5179
Emre Ozden <tlcj35@durham.ac.uk>
2025-01-23 16:47:14 +0000

    Added a nice little random recipe view

--- client/js/script.js
+++ client/js/script.js
@@ -6,8 +6,21 @@ let create_view = document.querySelector('#create_view');
 
 let CURRENT_VIEW = random_view;
 
+const API_URL = "http://localhost:3000"
+
 function switch_view(view) {
   CURRENT_VIEW.style = "display: none";
   CURRENT_VIEW = view;
-  view.style.display = ''
+  view.style.display = '';
 }
+
+
+function load_recipe_on_page_load() {
+  fetch("http://localhost:3000/api/recipe/random").then(res => res.json()).then(body => {
+    console.log(body);
+    document.querySelector("#recipe_title").innerHTML = body['dish_name']
+  })
+}
+
+
+window.onload = load_recipe_on_page_load;
