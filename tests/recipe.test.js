1d06f92f5c669ee8c0231dc1fc1726608d346f5a
Emre Ozden <tlcj35@durham.ac.uk>
2025-01-28 14:50:57 +0000

    :memo: Touched up formatting

--- tests/recipe.test.js
+++ tests/recipe.test.js
@@ -1,7 +1,8 @@
 const request = require("supertest");
 const app = require("../server/app.js");
 
-jest.mock("../server/data/recipes.json", () => [{
+jest.mock("../server/data/recipes.json", () => [
+  {
     id: 0,
     dish_name: "Risotto",
     author: "Gordon Ramsay",
@@ -9,54 +10,55 @@ jest.mock("../server/data/recipes.json", () => [{
     instructions: ["Make Risotto"],
     ingredients: ["Rice"],
     image_path: null,
-}, ]);
+  },
+]);
 
 describe("Testing Recipe-related API Endpoints", () => {
-    describe("GET /api/recipe/get", () => {
-        test("api.recipe.get should return a 200 response for valid input id", () => {
-            return request(app)
-                .get("/api/recipe/get")
-                .query({
-                    recipe_id: 0,
-                })
-                .expect(200);
-        });
-
-        test("api.recipe.get returns 400 for out-of-index id", () => {
-            return request(app)
-                .get("/api/recipe/get")
-                .query({
-                    recipe_id: 10000,
-                })
-                .expect(400);
-        });
-
-        test("api.recipe.get returns 400 for negative id", () => {
-            return request(app)
-                .get("/api/recipe/get")
-                .query({
-                    recipe_id: -1,
-                })
-                .expect(400);
-        });
-
-        test("api.recipe.get returns 400 for non-number id", () => {
-            return request(app)
-                .get("/api/recipe/get")
-                .query({
-                    recipe_id: "Hello, World!",
-                })
-                .expect(400);
-        });
+  describe("GET /api/recipe/get", () => {
+    test("api.recipe.get should return a 200 response for valid input id", () => {
+      return request(app)
+        .get("/api/recipe/get")
+        .query({
+          recipe_id: 0,
+        })
+        .expect(200);
     });
 
-    describe("GET /api/recipe/random", () => {
-        // Tests regarding the random route
+    test("api.recipe.get returns 400 for out-of-index id", () => {
+      return request(app)
+        .get("/api/recipe/get")
+        .query({
+          recipe_id: 10000,
+        })
+        .expect(400);
     });
 
-    describe("GET /api/recipe/search", () => {});
+    test("api.recipe.get returns 400 for negative id", () => {
+      return request(app)
+        .get("/api/recipe/get")
+        .query({
+          recipe_id: -1,
+        })
+        .expect(400);
+    });
 
-    describe("POST /api/recipe/create", () => {});
+    test("api.recipe.get returns 400 for non-number id", () => {
+      return request(app)
+        .get("/api/recipe/get")
+        .query({
+          recipe_id: "Hello, World!",
+        })
+        .expect(400);
+    });
+  });
 
-    describe("POST /api/recipe/img/add", () => {});
+  describe("GET /api/recipe/random", () => {
+    // Tests regarding the random route
+  });
+
+  describe("GET /api/recipe/search", () => {});
+
+  describe("POST /api/recipe/create", () => {});
+
+  describe("POST /api/recipe/img/add", () => {});
 });
